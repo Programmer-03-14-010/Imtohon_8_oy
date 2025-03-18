c83d4bf8c816a05d5270b2ba7498145be8a83c61
Abdullaxoja Bahodirov <mahkamovb54@gmail.com>
2025-03-18 14:12:03 +0500

    gitignore add

--- Config/settings.py
+++ Config/settings.py
@@ -1,17 +1,18 @@
 from datetime import timedelta
 from pathlib import Path
 
-from decouple import  config
+import environ
 
+env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = config('DEBUG')
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
