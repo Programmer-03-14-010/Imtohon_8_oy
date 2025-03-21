18cdc09bba397ded28a7c2e632fb8944aeeac6d8
Abdullaxoja Bahodirov <mahkamovb54@gmail.com>
2025-03-21 14:02:19 +0500

    change settings

--- Config/settings.py
+++ Config/settings.py
@@ -164,11 +164,10 @@ AUTH_USER_MODEL = 'app_conf.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
